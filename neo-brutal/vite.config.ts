2b3e8842ebdf96507a7a1b3db63879cfaea8533b
Tate McCauley <tatemccauley@gmail.com>
2025-12-01 11:20:15 -0700

    change to build for cloudflare pages

--- neo-brutal/vite.config.ts
+++ neo-brutal/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NB-BOM/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
